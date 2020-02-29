10fc0d19a685bf5c10466ca21600ec02a7ccf7fc
Haresh Kansara <kansarahar@gmail.com>
2020-02-29 16:27:11 -0500

    created new vue app

--- public/javascripts/nav.js
+++
@@ -1,7 +0,0 @@
-let navItems = document.querySelectorAll('.nav-item');
-for (let navItem of navItems) {
-    navItem.addEventListener('click', () => {
-        window.location.pathname = navItem.id;
-    });
-}
-
