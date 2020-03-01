7b1e181ddf3c32f6772f3b08862fda7f203bd2a3
Haresh Kansara <kansarahar@gmail.com>
2020-02-29 22:13:43 -0500

    got part of the navbar working

---
+++ src/components/NavBar/NavBarItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <li 
+    class="nav-item" 
+    v-bind:class="{ active: this.active }"
+    v-on:mouseover="setActive(true)"
+    v-on:mouseout="setActive(false)">
+    {{ this.name }}
+  </li>
+</template>
+
+
+<script>
+export default {
+  name: "NavBarItem",
+  props: {
+    name: {
+      type: String,
+      required: true,
+    },
+  },
+  data() {
+    return {
+      name: this.name,
+      active: false,
+    }
+  },
+  methods: {
+    setActive(bool) {
+      this.active = bool;
+    }
+  }
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import './src/assets/stylesheets/theme.scss';
+@import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+.active {
+  color: red;
+}
+</style>
---
+++ src/components/NavBar/NavBarMain.vue
@@ -0,0 +1,46 @@
+<template>
+  <nav>
+    <span id="logo">
+      <h1 id="kansar">Kansar</h1>
+      <h1 id="academy">Academy</h1>
+    </span>
+    <ul>
+      <NavBarItem name="Home"></NavBarItem>
+      <NavBarItem name="About"></NavBarItem>
+      <NavBarItem name="Projects"></NavBarItem>
+    </ul>
+  </nav>
+</template>
+
+
+<script>
+import NavBarItem from './NavBarItem';
+export default {
+  name: "NavBarMain",
+  components: { 
+    NavBarItem
+  },
+};
+</script>
+
+
+<style lang="scss" scoped>
+@import './src/assets/stylesheets/theme.scss';
+@import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
+#logo {
+  font-family: 'Montserrat';
+  font-size: 32px;
+  background-color: var(--main-nav-color);
+  display: flex;
+  padding: 5px 0 10px 20px;
+  
+}
+#kansar {
+  color: var(--text-color);
+  margin: 0;
+}
+#academy {
+  color: var(--alt-text-color);
+  margin: 0;
+}
+</style>
